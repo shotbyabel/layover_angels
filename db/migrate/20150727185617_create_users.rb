e567c6312407f5371ffd63f86d209428ccff860d
kimo5532 <shotbyabel@gmail.com>
2015-07-27 11:56:55 -0700

    created first User model, dabatabase and migration

---
+++ db/migrate/20150727185617_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :photo
+      t.text :bio
+
+      t.timestamps null: false
+    end
+  end
+end
