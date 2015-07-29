05e64cf5a4e879ec2e7183f16558937646efefe3
kimo5532 <shotbyabel@gmail.com>
2015-07-29 16:38:22 -0700

    added couple buttons and fixed links for editing profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,21 @@ def create
     end  
    end
 
+######
+ # def create
+ #    user = User.find_by(email: params[:session][:email].downcase)
+ #    if user && user.authenticate(params[:session][:password])
+ #      log_in user
+ #      remember user
+ #      redirect_to user
+ #    else
+ #      flash.now[:danger] = 'Invalid email/password combination'
+ #      render 'new'
+ #    end
+ #  end
+  ############
+
+
  # def new
  #  user = User.find_by(email: params[:email]
 
